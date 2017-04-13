e3e223754ac2dada91efcfb408affe638133ac70
Alsayedahmad <eho07@ostml0222458.bskyb.com>
2017-04-13 09:13:32 +0100

    First commit

---
+++ file1.py
@@ -0,0 +1,25 @@
+my_variable = 10
+my_int = 7
+my_float = 2.75
+my_bool = True
+
+my_int = 3
+print my_int
+
+#This is a single line comment
+
+"""
+This is a
+multi line
+comment
+"""
+
+count = 255 + 481
+print count
+
+my_exponent = 10**3
+print my_exponent
+
+my_modulus = 5 % 2
+print my_modulus
+
